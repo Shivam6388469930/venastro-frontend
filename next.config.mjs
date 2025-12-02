8e22ec3c15a118e197a96afa9c5a35bcd4609755
Shivam Agrahari <agraharishivam6388@gmail.com>
2025-12-02 17:11:27 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export', // enable static export
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
